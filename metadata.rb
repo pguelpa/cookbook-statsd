1c73cfdbf25a3db79b7be1d916290be83d65d6c5
Dan van den Berg <dan@shopkeep.com>
2013-08-01 14:46:25 -0400

    Version bumped up to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "Jon Wood"
-maintainer_email "jon@blankpad.net"
+maintainer       "Daniel van den Berg"
+maintainer_email "dan@shopkeep.com""
 license          "Apache 2.0"
 description      "Installs/Configures statsd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.1.0"
 
 depends "build-essential"
 depends "git"
